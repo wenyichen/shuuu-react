f7faa77511a97558e9c13fa4cd0dadcaf11fe356
Wenyi Chen <wenyichen@outlook.com>
2018-02-20 12:06:41 -0800

    deleted extra index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
-import './App.css';
 import routes from './route.js';
 
 class App extends Component {
