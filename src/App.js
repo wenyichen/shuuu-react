1f460a3743e093cb0a9b1df2473ca7804b69b6ef
Wenyi Chen <wenyichen@outlook.com>
2018-02-20 12:26:17 -0800

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import {BrowserRouter,  Route } from "react-router-dom";
 import routes from './route.js';
 
 class App extends Component {
   render() {
     const routeComponents = routes.map(({path, component}, key) => <Route exact path={path} component={component} key={key} />);
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/repo-name">
         <div>
           {routeComponents}
         </div>
