9b3e744530e0f7bdab02826187b2a61f730c16ee
Wenyi Chen <wenyichen@outlook.com>
2018-02-20 21:19:50 -0800

    fixed a bunch of css

--- src/Common/Navbar.js
+++ src/Common/Navbar.js
@@ -21,14 +21,14 @@ class Navbar extends Component {
     render() {
         return (
             <div>
-                <Title display={this.props.title} />
                 <div id="bar">
+                    <Title display={this.props.title} />
                     <div id="nav">
                         {/* If logged in display My Account/MyTrips
                         else display Login/Signup */}
                         <ul>
-                            <Link to="/account"><li>My Account</li></Link>
-                            <Link to="/trips"><li>My Trips</li></Link>
+                            <Link to="/account"><li><span>My Account</span></li></Link>
+                            <Link to="/trips"><li><span>My Trips</span></li></Link>
                         </ul>
                     </div>
                 </div>
--- src/Info-Driver.js
+++ src/Info-Driver.js
@@ -13,43 +13,43 @@ class InfoDriver extends Component {
                 <Navbar />
                 <div id="overlay">
                 </div>
-                <div class="middlebox" id="info-box">
+                <div className="middlebox" id="info-box">
                     <div id="middlebox-topbar">
                         <p>Find riders:</p>
                     </div>
                     <form id="info-form" method="GET" action="./trips.html">
-                        <div class="text-input">
-                            <label for="_depart">Departing From:</label>
+                        <div className="text-input">
+                            <label htmlFor="_depart">Departing From:</label>
                             <Autocomplete id="_depart" type="text" name="depart" required/>
                         </div>
-                        <div class="text-input">
-                            <label for="_dest">Destination:</label>
+                        <div className="text-input">
+                            <label htmlFor="_dest">Destination:</label>
                             <Autocomplete id="_dest" type="text" name="dest" required/>
                         </div>
-                        <div class="date-input">
-                            <label for="_date">Date of Trip:</label>
+                        <div className="date-input">
+                            <label htmlFor="_date">Date of Trip:</label>
                             <input type="date" id="_date" name="date" required/>
                         </div>
-                        <div class="text-input">
-                            <label for="_type">Type of Car:</label>
+                        <div className="text-input">
+                            <label htmlFor="_type">Type of Car:</label>
                             <input type="text" id="_type" name="type" required/>
                         </div>
-                        <div class="text-input">
-                            <label for="_carNo.">Car NO.:</label>
+                        <div className="text-input">
+                            <label htmlFor="_carNo.">Car NO.:</label>
                             <input type="text" id="_carNo." name="carNo." required/>
                         </div>
-                        <div class="num-input">
-                            <label for="_seats">Number of Seats:</label>
+                        <div className="num-input">
+                            <label htmlFor="_seats">Number of Seats:</label>
                             <input type="number" id="_seats" name="seats" min="1" max="7"
                             placeholder="4" required/>
                         </div>
-                        <div class="tel-input">
-                            <label for="_tel">Phone Number:</label>
+                        <div className="tel-input">
+                            <label htmlFor="_tel">Phone Number:</label>
                             <input type="tel" id="_tel" name="tel" placeholder="(XXX)-XXX-XXXX"
                             pattern="\d{4}-\d{3}-\d{4}$" required/>
                         </div>
-                        <div class="note-input">
-                            <label id="notes_label" for="_notes">Notes:</label>
+                        <div className="note-input">
+                            <label id="notes_label" htmlFor="_notes">Notes:</label>
                             <textarea id="_notes" rows="5" name="notes"></textarea>
                         </div>
                         <button type="submit" id="continue-button">Continue</button>
