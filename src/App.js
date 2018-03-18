f91c42c32f4824c18436bf5d25de74d251d7f936
Wenyi Chen <wenyichen@outlook.com>
2018-03-18 00:22:44 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import { BrowserRouter,  Route } from "react-router-dom";
-import routes from './route.js';
-import './css/fonts.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import routes from "./route.js";
+import "./css/fonts.css";
 
 class App extends Component {
-  render() {
-    const routeComponents = routes.map(({path, component}, key) => <Route exact path={path} component={component} key={key} />);
-    return (
-      <Provider store={store}>
-        <BrowserRouter basename="/shuuu-react">
-          <div>
-            {routeComponents}
-          </div>
-        </BrowserRouter>
-      </Provider>
-    );
-  }
+    render() {
+        const routeComponents = routes.map(({ path, component }, key) => (
+            <Route exact path={path} component={component} key={key} />
+        ));
+        return (
+            <BrowserRouter basename="/shuuu-react">
+                <div>{routeComponents}</div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 import shuuuApp from "./reducers";
-let store = createStore(todoApp);
+
+let store = createStore(shuuuApp);
 
 ReactDOM.render(
     <Provider store={store}>
