0743c7a67a78ad209974bc772341cda9a5cea828
Wenyi Chen <wenyichen@outlook.com>
2018-03-18 00:57:04 -0700

    updated account actions

--- src/actions/Account-Actions.js
+++ src/actions/Account-Actions.js
@@ -1,16 +1,42 @@
 import * as types from './Action-Types';
+import accountapi from '../api/mockAccountApi';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 /*
  * action creators
  */
-export function create_account(account) {
-  return {type: types.CREATE_ACCOUNT_SUCCESS, account}
- }
-
-export function load_account(account) {
-  return {type: types.LOAD_ACCOUNT_SUCCESS, account}
+export function loadAccountSuccess(account) {
+  return { type: types.LOAD_COURSES_SUCCESS, account};
 }
 
-export function edit_account(account) {
-  return {type: types.EDIT_ACCOUNT_SUCCESS, account}
+export function createAccountSuccess(account) {
+  return {type: types.CREATE_COURSE_SUCCESS, account};
+}
+
+export function updateAccountSuccess(account) {
+  return {type: types.UPDATE_ACCOUNT_SUCCESS, account};
+}
+
+export function saveAccount(account) {
+  return function (dispatch, getState) {
+    dispatch(beginAjaxCall());
+    return courseApi.saveAccount(account).then(account => {
+      account.id ? dispatch(updateAccountSuccess(account)) :
+        dispatch(createAccountSuccess(account));
+    }).catch(error => {
+      dispatch(ajaxCallError(error));
+      throw(error);
+    });
+  };
+ }
+
+export function loadAccount(account) {
+  return function(dispatch) {
+    dispatch(beginAjaxCall());
+    return accountApi.getAllAccounts().then(account => {
+      dispatch(loadAccountsSuccess(account));
+    }).catch(error => {
+      throw(error);
+    });
+  };
 }
